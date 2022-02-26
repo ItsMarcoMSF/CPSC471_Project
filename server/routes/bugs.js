42e67d4790c4e6423bff6a54343afd8146267332
banhducnonggg <banhducnonggg@gmail.com>
2022-02-26 16:12:09 -0700

    getAllBugs.js

--- server/routes/bugs.js
+++ server/routes/bugs.js
@@ -4,6 +4,7 @@ import { createBugs } from "../controllers/createBugs.js";
 import { createProjects } from "../controllers/createProjects.js";
 import { getBugsByID } from "../controllers/getBugsByID.js";
 import { getProjects } from "../controllers/getProjects.js";
+import { getAllBugs } from "../controllers/getAllBugs.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.post("/bugs", createBugs);
 router.post("/projects", createProjects);
 router.get("/bugs/:bugID", getBugsByID);
 router.get("/projects/:projectID", getProjects);
+router.get("/bugs", getAllBugs);
 
 export default router;
