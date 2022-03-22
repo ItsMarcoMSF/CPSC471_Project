f6d94c3cada30777aa02a09c65a3d175bc1d71ac
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2022-03-21 20:26:49 -0600

    added API for User

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import User from "./models/user.js";
 import Routes from "./routes/bugs.js";
 
 const app = express();
-dotenv.config();
+dotenv.config({path:'server/.env'});
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
@@ -20,6 +20,7 @@ app.use(cors());
 // var jwt = require("jsonwebtoken");
 // const bcrypt = require("bcrypt");
 // const User = require("./models/user.js")
+
 
 function verifyJWT(req, res, next) {
   const token = req.headers["x-access-token"]?.split(" ")[1];
@@ -108,7 +109,7 @@ app.get("/isUserAuth", verifyJWT, (req, res) => {
   res.json({ isLoggedIn: true, username: req.user.username });
 });
 
-// console.log(process.env.CONNECTION_URL);
+//console.log(process.env.CONNECTION_URL);
 
 const PORT = process.env.PORT || 5000;
 
