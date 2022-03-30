3d0be86372464d2e89ad0376e23a23724c69c39b
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2022-03-29 21:36:40 -0600

    ProjectPage initial commit

--- client/src/components/ProjectPage/ProjectPage.js
+++ client/src/components/ProjectPage/ProjectPage.js
@@ -3,13 +3,29 @@ import React from "react";
 import "./ProjectPage.css";
 
 const ProjectPage = ({ project }) => {
+
   return (
     <div className="project-wrapper">
       {project ? (
-        <div>
-          <h2>{project.name}</h2>
-          <p>There is a project</p>
+        <><div id="rectangle-small-left">
+          <h2>Project Progress:</h2>
         </div>
+        <div id="rectangle-small-right">
+            <h2>Your Progress:</h2>
+        </div>
+        <div id="rectangle-large">
+            <h2>Upcoming Task</h2>
+            <p>{project.bugs[0].name}</p>
+            <p>{project.bugs[0].deadline}</p>
+        </div>
+        <div id="rectangle-large">
+            <h2>Project Resources</h2>
+            <h3>Managers</h3>
+            <p>{project.manager.name}</p>
+            <h3>Developers</h3>
+            <p>{project.developer[0].name}</p>
+        </div>
+        </>
       ) : (
         <h2>There is no project</h2>
       )}
