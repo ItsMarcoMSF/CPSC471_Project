62ce5fec1cc62c318de504ca5df6f84fcd3d9e2e
banhducnonggg <banhducnonggg@gmail.com>
2022-04-07 23:37:46 -0600

    merge

--- .history/client/src/components/BugsPage/BugsPage_20220407231829.js
+++ .history/client/src/components/BugsPage/BugsPage_20220407231829.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import { useMediaQuery } from 'react-responsive'
 
 import "./BugsPage.css";
 
@@ -10,10 +9,6 @@ import BugLists from "../BugLists/BugLists";
 import ReportBtn from "../ReportBtn/ReportBtn";
 
 const BugsPage = ({ project, switchToProject }) => {
-
-  // const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' });
-  // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
-
   const [isLoaded, setIsLoaded] = useState(false);
   const [getBugs, setGetBugs] = useState([]);
 
@@ -102,31 +97,6 @@ const BugsPage = ({ project, switchToProject }) => {
     }
   };
 
-  // const fetchDev = async () => {
-  //   const projID = project._id;
-  //   const payload = {
-  //       method: "GET",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //         "x-access-token": localStorage.getItem("token"),
-  //       },
-  //   };
-
-  //   try {
-  //     const res = await fetch(
-  //       `http://localhost:5000/projects/${projID}/bugs`,
-  //       payload,
-  //     );
-  //     const bug = await res.json();
-  //     setIsLoaded(true);
-  //     setGetBugs(bug);
-  //     console.log(bug);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
   useEffect(() => {
     fetchBugs();
   }, []);
@@ -160,19 +130,19 @@ const BugsPage = ({ project, switchToProject }) => {
 
   return (
     <div className="bugs-wrapper">
-      {/* {isDesktopOrLaptop && <p>You are a desktop or laptop</p>}
-      {isTabletOrMobile && <p>You are a tablet or mobile</p>} */}
       <button className="back-btn" onClick={switchToProject}>
         Go back
       </button>
       {getBugs.length > 0 ? (
         <div>
+          {/* <h2>{bugs.name}</h2> */}
           <h2 className="bugs-reported">Bugs reported:</h2>
           <nav>
             <ul>
               <BugLists bugs={getBugs} toggleDetail={toggleDetail} />
             </ul>
           </nav>
+          {/* <ReportBtn/> */}
           <input
             className="rptbtn"
             type="button"
@@ -225,7 +195,11 @@ const BugsPage = ({ project, switchToProject }) => {
                       type="date"
                       placeholder="Bug's deadline"
                     />
+                    {/* <p>Bug status</p> */}
                     <p
+                      // className="enter-detail"
+                      // value={sendBug.status}
+                      // onChange={(e) => handle(e)}
                       value="Unresolved"
                       id="status"
                     >Bug status: Unresolved</p>
@@ -238,6 +212,7 @@ const BugsPage = ({ project, switchToProject }) => {
               handleClose={togglePopup}
             />
           )}
+          {/* <ReportBtn/> */}
         </div>
       ) : (
         <div>
@@ -289,7 +264,11 @@ const BugsPage = ({ project, switchToProject }) => {
                       type="date"
                       placeholder="Bug's deadline"
                     />
+                    {/* <p>Bug status</p> */}
                     <p
+                      // className="enter-detail"
+                      // value={sendBug.status}
+                      // onChange={(e) => handle(e)}
                       value="Unresolved"
                       id="status"
                     >Bug status: Unresolved</p>
